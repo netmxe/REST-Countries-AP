c5943e9ebb98bb92490f80a65568d63e128a8bd6
NetMx <e.netmx@outlook.com>
2024-11-29 21:40:04 +0100

    Changement adaptater

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter(), // Utilisez l'adaptateur Vercel
 		alias: {
-			$components: path.resolve('./src/components') // Crée l'alias pour "src/components"
+			$components: path.resolve('./src/components') // Alias pour src/components
+		},
+		prerender: {
+			entries: ['*'] // Prérendre toutes les pages si nécessaire
 		}
 	}
-
 };
 
 export default config;
